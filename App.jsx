b114cc904c921ca4fb4819084d35d7fcf55e28fa
PUNEET KUMAR <kumarmahesh3594@gmail.com>
2026-03-10 17:43:17 +0530

    App component layout and formatting

--- App.jsx
+++ App.jsx
@@ -1,32 +1,32 @@
 import React, { useState } from "react";
 import "./App.css";
+import ProductList from "./components/ProductList";
 
 function App() {
 
-const [showProductList,setShowProductList] = useState(false);
+  const [showProductList, setShowProductList] = useState(false);
 
-return(
+  return (
+    <div>
 
-<div>
+      {!showProductList && (
 
-{!showProductList && (
+        <div className="landing-page">
 
-<div className="landing-page">
+          <h1>Welcome to Paradise Nursery</h1>
 
-<h1>Welcome to Paradise Nursery</h1>
+          <button onClick={() => setShowProductList(true)}>
+            Get Started
+          </button>
 
-<button onClick={()=>setShowProductList(true)}>
-Get Started
-</button>
+        </div>
 
-</div>
+      )}
 
-)}
+      {showProductList && <ProductList />}
 
-</div>
-
-)
-
+    </div>
+  );
 }
 
 export default App;
